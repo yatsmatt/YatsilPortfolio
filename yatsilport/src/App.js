7cf2adc59074f6f047e48da3e0b4ecffba3a0b5d
yatsilmatt <yatsilmatt@gmail.com>
2023-08-22 17:46:59 +0300

    set up routes and components

--- yatsilport/src/App.js
+++ yatsilport/src/App.js
@@ -1,13 +1,31 @@
 import "./App.css";
-import MyNavBar from "./component/MyNavBar";
+import { Container, Grid } from "@mui/material";
+import Profile from "./component/Profile";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import Router from "./Routes/Router";
+
 function App() {
   return (
-    <div className="App">
-      hello
-      <div>
-        <MyNavBar />
-      </div>
-    </div>
+    <Container>
+      <Grid container>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          lg={4}
+          md={3}
+          style={{ backgroundColor: "blue" }}
+        >
+          <Profile />
+        </Grid>
+        <Grid item xs style={{ backgroundColor: "red" }}>
+          <Header />
+          <Router />
+          <Footer />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
