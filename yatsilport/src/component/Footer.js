c16ec2e2acb206801231848abd27633f5394a4b0
yatsilmatt <yatsilmatt@gmail.com>
2023-08-31 17:11:39 +0300

    footer finished

--- yatsilport/src/component/Footer.js
+++ yatsilport/src/component/Footer.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Typography } from "@mui/material";
 
+import "./Footer.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer_left">
+        <Typography> Yatsil Mattok personal website</Typography>
+      </div>
+      <div className="footer_right">
+        <Typography className="me">
+          Desinged and Developed by{" "}
+          <a href="/" target="_blank">
+            Yatsil Mattok
+          </a>
+        </Typography>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
