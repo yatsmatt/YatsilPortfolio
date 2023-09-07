b976461bedaeae86d9ac7ce639b9f84ecd98672b
yatsilmatt <yatsilmatt@gmail.com>
2023-09-07 16:00:34 +0300

    cards of project and desinge in place

--- README.md
+++
@@ -1,2 +0,0 @@
-# YatsilPortfolio
-my personal website
---
+++ yatsilport/src/utils/Project.js
@@ -0,0 +1,20 @@
+export default {
+  title: "My Portfolio",
+  content:
+    "Welcome to my portfolio Here you will be able to see the last project that I've been doing but don't worry I'm a freak of Technology and soon I will upload different projects on different technologies Meanwhile you can check my GitHub or my GitLab",
+  Projects: [
+    // { name: "", sum: "", img: "", link: "" },
+    {
+      name: "Cofeds",
+      sum: "My associate Peleg Swissa and I made a full-stack App for small businesses that lets the clients make online purchases and for businesses, side control the inventory using React and Django and MySQL",
+      img: "https://pas-wordpress-media.s3.amazonaws.com/content/uploads/2014/10/stateofsmallbusiness-e1412356629429-653x339.jpg",
+      link: "https://gitlab.com/PelegSwisa6/co-feds",
+    },
+    {
+      name: "My Personal Website",
+      sum: "welcome to my portfolio This is the website that I designed from stretch and implemented using react material Are you and Bootstrap This website is my visit card",
+      img: "https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL3NwYWNlLXNtZWxsLTIuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjo4Mjh9LCJ0b0Zvcm1hdCI6ImF2aWYifX0=",
+      link: "",
+    },
+  ],
+};
